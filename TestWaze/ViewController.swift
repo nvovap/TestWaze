406740ced3d04619711bffd2eb9318cefe5d22f0
Vladimir Nevinniy <nvovap@gmail.com>
2017-12-27 14:20:43 +0200

    Initial Commit

---
+++ TestWaze/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestWaze
+//
+//  Created by Владимир Невинный on 27.12.2017.
+//  Copyright © 2017 Владимир Невинный. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
