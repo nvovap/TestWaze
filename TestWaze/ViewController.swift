ebc5db7d43c3a8fa1a58a044da16348e43390159
Vladimir Nevinniy <nvovap@gmail.com>
2017-12-27 19:06:02 +0200

    first commit

--- TestWaze/ViewController.swift
+++ TestWaze/ViewController.swift
@@ -9,6 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    func navigateToLatitude(_ latitude: Double, longitude: Double)  {
+        
+        
+        
+        if UIApplication.shared.canOpenURL(URL(string: "waze://")!) {
+            
+            if let urlStr = URL(string: "https://waze.com/ul?ll=\(latitude),\(longitude)&navigate=yes") {
+                UIApplication.shared.open(urlStr, options: [:], completionHandler: nil)
+            }
+            
+        } else {
+            UIApplication.shared.open(URL(string: "http://itunes.apple.com/us/app/id323229106")!, options: [:], completionHandler: nil)
+        }
+    }
+    
+    @IBAction func onButton(_ sender: Any) {
+        //45.6906304,-120.810983
+        navigateToLatitude(47.838800000000006, longitude: 35.139567)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
